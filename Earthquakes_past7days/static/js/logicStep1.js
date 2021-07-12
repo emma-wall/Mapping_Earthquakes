e076b4663f0f70e5a3f848767a6f5e86e552e1e7
emma-wall <80648379+emma-wall@users.noreply.github.com>
2021-07-11 23:26:22 -0400

    adding earthquake files

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,40 @@
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create the dark view tile layer
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid', {
+    center: [39.5, -98.5], 
+    zoom: 3, 
+    layers:[streets]
+});
+
+
+// Create a base layer that holds both maps
+
+let baseMaps = {
+    Satellite: satellite,
+    Streets: streets
+};
+
+// Pass our map layers into our layers controll and add the layers
+L.control.layers(baseMaps).addTo(map);
+
+// Grabbing our GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data){
+    console.log(data);
+    //Creating a  GeoJSON layer with the retrieved data
+    L.geoJson(data).addTo(map);
+});
+
